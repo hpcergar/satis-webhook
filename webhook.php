dd83855ac3adbe0e388a8d6e2815f7be134a225a
hpcer <hp.cerveragarcia@dbit.lu>
2016-07-27 08:59:08 +0200

    [IMP] Unattended version

--- webhook.php
+++ webhook.php
@@ -1,8 +1,19 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
 
+$unattended = isset($_REQUEST['unattended']) ? true : false;
+
+if($unattended){
+// Let request sender start
+    ignore_user_abort(true);
+    set_time_limit(0);
+    ob_start();
+}
+
 use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
+
+header('Content-Type: text/html; charset=utf-8');
 
 if (!file_exists(__DIR__.'/config.yml')) {
     echo "Please, define your satis configuration in a config.yml file.\nYou can use the config.yml.dist as a template.";
@@ -32,11 +43,21 @@ if (!file_exists($config['webroot'])) {
 }
 
 if (!empty($errors)) {
+    header("{$_SERVER['SERVER_PROTOCOL']} 500 Internal Server Error", true, 500);
     echo 'The build cannot be run due to some errors. Please, review them and check your config.yml:'."\n";
     foreach ($errors as $error) {
         echo '- '.$error."\n";
     }
     exit(-1);
+}
+
+if($unattended){
+    echo 'Unattended version, config OK';
+    header('Connection: close');
+    header('Content-Length: '.ob_get_length());
+    ob_end_flush();
+    ob_flush();
+    flush();
 }
 
 $command = sprintf('%s build %s %s', $config['bin'], $config['json'], $config['webroot']);
@@ -54,4 +75,4 @@ $exitCode = $process->run(function ($type, $buffer) {
     }
 });
 
-echo "\n\n" . ($exitCode === 0 ? 'Successful rebuild!' : 'Oops! An error occured!') . "\n";
+echo "\n\n" . ($exitCode === 0 ? 'Successful rebuild!' : 'Oops! An error occurred!') . "\n";
